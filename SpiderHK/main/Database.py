eb14d9ba8df59c9ac8efd5e577a4f634fa793aca
HiKari <361914599@qq.com>
2018-03-27 23:04:30 +0800

    Update Database.py

--- SpiderHK/main/Database.py
+++ SpiderHK/main/Database.py
@@ -10,8 +10,8 @@ def config():  # Database Config
         'host': '127.0.0.1',
         'port': 3306,
         'user': 'root',
-        'password': 'huangdq',
-        'db': 'spider_python',
+        'password': 'yourpasswor',
+        'db': 'yourdatabase',
         'charset': 'utf8',
         'cursorclass': pymysql.cursors.DictCursor,
     }
